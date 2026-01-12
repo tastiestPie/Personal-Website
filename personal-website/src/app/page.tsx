eecc93216b1c467c1149ee66711c2ac6819acfb3
Gabriel Que <gabeque11@gmail.com>
2026-01-12 10:34:07 -0500

    Updated to patch NextJS Vulnerability

--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -3,7 +3,14 @@ import Navbar from "./components/navbar";
 export default function Home() {
   return (
     <>
+      <div>
       <h1>Hi There!</h1>
+      </div>
+
+      <div>
+        <img src="20230519_171455.jpg"></img>
+        <h3>I'm a programmer, software developer, and hobbist game developer.</h3>
+      </div>
     </>
   );
 }
