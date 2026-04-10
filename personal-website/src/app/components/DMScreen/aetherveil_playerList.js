f11f9a0b779abc47822896277bb381fd6a25998f
Gabriel Que <gabeque11@gmail.com>
2026-04-09 20:22:40 -0400

    Pushing player list

---
+++ personal-website/src/app/components/DMScreen/aetherveil_playerList.js
@@ -0,0 +1,37 @@
+export const Player_List = [
+    {
+        dndName: "Aqu",
+        irlName: "Gavin",
+        hp: 31,
+        race: "Water Plasmoid",
+        class: "Monk",
+    },
+    {
+        dndName: "Binky Fiasco",
+        irlName: "Carlo",
+        hp: 40,
+        race: "Genasi",
+        class: "Fighter",
+    },
+    {
+        dndName: "Pres Ciseshooter",
+        irlName: "Dean",
+        hp: 32,
+        race: "Human",
+        class: "Fighter",
+    },
+    {
+        dndName: "Ryujin",
+        irlName: "Seth",
+        hp: 31,
+        race: "Human",
+        class: "Warlock / Sorcerer",
+    },
+    {
+        dndName: "Volt",
+        irlName: "David",
+        hp: 23,
+        race: "Dragonborn",
+        class: "Cleric",
+    },
+];
